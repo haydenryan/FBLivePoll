4b1fda28124261097987005335f40a6c62f1269a
HaydenRyan <hayden@gennext.net.au>
2016-11-18 22:44:04 +1100

    Get Video ID from variable, pass JSON data on
    If videoid variable is set in request, that Video ID will be used -
    otherwise the hardcoded value will be used. If json variable is set,
    the whole of the JSON response will be echoed back instead.

--- reactions.php
+++ reactions.php
@@ -4,8 +4,11 @@
 //  Set the ID of the Live Video below,  //
 //  along with your app's access token   //
 ///////////////////////////////////////////
-
+if (isset($_GET['videoid'])){
+	$videoid=$_GET['videoid'];
+} else {
 $videoid="VIDEO_ID_HERE";                                    // The ID of the Live Video
+}
 $access_token="ACCESS_TOKEN_HERE";   // Your Facebook app's access token in the format 1111111111111111|X1xX11xX_XXXxXxXXX1xXXx1XXX
 
 
@@ -15,7 +18,7 @@ set_time_limit(128); // Increase the standard time limit
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_URL, 'https://graph.facebook.com/v2.8/?id='.$videoid.'&fields=reactions.type(LOVE).limit(0).summary(total_count).as(reactions_love),reactions.type(LIKE).limit(0).summary(total_count).as(reactions_like),reactions.type(ANGRY).limit(0).summary(total_count).as(reactions_angry),reactions.type(WOW).limit(0).summary(total_count).as(reactions_wow),reactions.type(HAHA).limit(0).summary(total_count).as(reactions_haha)&access_token='.$access_token.'');
+curl_setopt($ch, CURLOPT_URL, 'https://graph.facebook.com/v2.8/?id='.$videoid.'&fields=reactions.type(LOVE).limit(0).summary(total_count).as(reactions_love),reactions.type(LIKE).limit(0).summary(total_count).as(reactions_like),reactions.type(ANGRY).limit(0).summary(total_count).as(reactions_angry),reactions.type(WOW).limit(0).summary(total_count).as(reactions_wow),reactions.type(HAHA).limit(0).summary(total_count).as(reactions_haha),reactions.type(SAD).limit(0).summary(total_count).as(reactions_sad)&access_token='.$access_token.'');
 $result = curl_exec($ch);
 curl_close($ch);
 
@@ -27,7 +30,13 @@ $wow=$obj->reactions_wow->summary->total_count;
 $haha=$obj->reactions_haha->summary->total_count;
 $like=$obj->reactions_like->summary->total_count;
 $angry=$obj->reactions_angry->summary->total_count;
+$sad=$obj->reactions_sad->summary->total_count;
 
+// just echo the JSON data if the json variable is set
+if (isset($_GET['json'])){
+		echo $result;
+		die;
+	}
 
 // If requesting an INDIVIDUAL reaction (ie. reaction variable is set in URL), display only the quantity of that specific reaction
 if (isset($_GET['reaction'])){
@@ -46,9 +55,9 @@ echo $wow;
 
 } elseif ($reaction=="angry"){
 	echo $angry;
-
+} elseif ($reaction=="sad"){
+	echo $sad;
 }
-
 // Otherwise, if the reaction variable is NOT set in the URL, display the quantity of all reactions - this is good for testing purposes
 } else {
 echo "LOVE: ";
